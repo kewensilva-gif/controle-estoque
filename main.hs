16b780dabb56a9c385961acd69a242de97a02798
KEWEN SILVA <kewensilva58@gmail.com>
2024-08-13 20:27:10 -0300

    [feat: inserção do menu do usuário]

--- main.hs
+++ main.hs
@@ -2,8 +2,28 @@ module Main where
 import Estoque
 import Produto
 
+opcoes :: String
+opcoes = "1 - Retorna nome\n2 - Retorna Quantidade\n0 - Sair"
+
+menu :: Int -> IO()
+menu op = case op of
+    1 -> do
+        putStrLn "Digite o id do produto"
+        id <- getLine
+        putStrLn (getNomeProduto listaProdutos (read id :: Int))
+        repeatMenu
+    0 ->
+        do
+            putStrLn "Encerra"
+
+repeatMenu :: IO()
+repeatMenu = do
+    putStr opcoes
+    putStr "Digite a opção desejada: "
+    op <- getLine
+    menu (read op :: Int)
+
+
 main :: IO()
 main = do
-    putStrLn "Digite o id do produto"
-    id <- getLine
-    putStrLn (getNomeProduto listaProdutos (read id :: Int))
+    repeatMenu
